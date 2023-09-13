9350c22d3b6f0c6b811d970adf857c4858215010
spiggen <jonasvilgotlotberg@hotmail.se>
2023-09-13 13:42:04 +0200

    forgot to add Euler-forwards method

---
+++ Euler_forward.m
@@ -0,0 +1,14 @@
+function [x,y] = Euler_forward(x,y, fcn, delta_t)
+if nargout(fcn) == 1
+x = x + delta_t;
+y = y + fcn(x,y)*delta_t;
+
+elseif nargout(fcn) == 2
+[delta_x, delta_y] = fcn(x,y);
+
+x = x + delta_x*delta_t;
+y = y + delta_y*delta_t;
+
+end
+
+end
